72795ab12c23dec6b3bf198e29c3954241501e58
allenxz <13226271061@163.com>
2019-11-27 19:46:31 +0800

    uniform color

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';//暂时先全部引入
+import './theme/index.css';
 
 Vue.use(ElementUI);
 Vue.config.productionTip = false;
