392034e2f5e76b6327073cc53f1d22949eee7c61
allenxz <13226271061@163.com>
2019-11-28 21:38:59 +0800

    add function to read and sava the login state

--- src/App.vue
+++ src/App.vue
@@ -11,3 +11,16 @@
   padding: 0;
 }
 </style>
+<script>
+export default {
+  created (){
+     //在页面加载时读取localStorage里的状态信息
+    localStorage.getItem("state") && this.$store.replaceState(JSON.parse(localStorage.getItem("state")));
+    
+    //在页面刷新时将vuex里的信息保存到localStorage里
+    window.addEventListener("beforeunload",()=>{
+      localStorage.setItem("state",JSON.stringify(this.$store.state))
+    })
+  }
+}
+</script>
