56b6bf1b11e2b29dc369967612b3dfe06cc14705
allenxz <13226271061@163.com>
2019-11-28 21:37:55 +0800

    use vuex to manage login state

--- src/store/index.js
+++ src/store/index.js
@@ -4,12 +4,24 @@ import Vuex from 'vuex';
 
 Vue.use(Vuex)
 
+function storeLocalStore (state) {
+  window.localStorage.setItem("state",JSON.stringify(state));
+}
+
 export default new Vuex.Store({
   state: {
+    userInfo: null
   },
   mutations: {
+    LOGINED(state, data){
+      state.userInfo = data;
+      storeLocalStore (state);
+    }
   },
   actions: {
+    logined({commit}, data) {
+      commit('LOGINED', data);
+    }
   },
   modules: {
   }
