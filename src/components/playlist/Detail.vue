4099ec592cf271daf3370d5b1b7a863cc5f7696e
allenxz <13226271061@163.com>
2019-11-30 11:43:13 +0800

    add new module

---
+++ src/components/playlist/Detail.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div v-if="playlist">
+      <ul v-for="(item, index) in playlist.tracks" :key="index">
+        <li>{{item.name}}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import { getPlaylistDetailByLid } from '@/api/playlist.js';
+
+export default {
+  name: 'Detail',
+  data() {
+    return {
+      playlist:null
+    }
+  },
+  created() {
+    let lid = this.$route.query.lid;
+    getPlaylistDetailByLid(lid).then(res => {
+      this.playlist = res;
+    })
+  }
+}
+
+</script>
+<style scoped lang='less'>
+</style>
