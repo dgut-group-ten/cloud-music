6cbb8afcbb6e4030e246f6202de15baf9e4f4010
allenxz <13226271061@163.com>
2019-12-02 11:20:46 +0800

    add new module

---
+++ src/components/common/Comment.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="comment">
+    评论
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'comment'
+}
+
+</script>
+<style scoped lang='less'>
+  .comment{
+    margin-top: 20px;
+  }
+</style>
