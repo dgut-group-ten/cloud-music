0e028f522b5a1baa657c03726ff99d33ddf4349d
allenxz <13226271061@163.com>
2019-12-02 11:21:05 +0800

    add new module

---
+++ src/components/common/Foot.vue
@@ -0,0 +1,25 @@
+<template>
+  <footer>
+    Copyright © 2019 DGUT Group Ten. All Rights Reserved.
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Foot'
+}
+
+</script>
+<style scoped lang='less'>
+@import '../../styles/common.less';
+  footer{
+    height: 60px;
+    width: 100%;
+    background: @footer-bg-color;
+    color: @footer-text-color;
+    font-size: @fs-s;
+    text-align: center;
+    line-height: 60px;
+    vertical-align: middle;
+  }
+</style>
