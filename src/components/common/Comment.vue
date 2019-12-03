69f19db054aec9351e853971f22cc2d2e8be3431
allenxz <13226271061@163.com>
2019-12-03 15:35:25 +0800

    design layout

--- src/components/common/Comment.vue
+++ src/components/common/Comment.vue
@@ -1,17 +1,112 @@
 <template>
   <div class="comment">
-    评论
+    <h1>评论</h1>
+    <span>共1条评论</span>
+    <!-- 编辑区 -->
+    <div class="comment-edit">
+      <el-input
+        type="textarea"
+        placeholder="期待你的精彩故事"
+        v-model="textarea"
+        maxlength="300"
+        show-word-limit
+        :autosize="{ minRows: 4, maxRows: 6}"
+      >
+      </el-input>
+      <el-button class="comment-submit" size="small" type="primary">发表评论</el-button>
+    </div>
+    <!-- 展示区 -->
+    <div class="comment-show">
+      <h2>最新评论</h2>
+      <span>(1)</span>
+      <ul class="comment-list">
+        <li class="comment-item">
+          <img class="avatar" src="https://gss3.bdstatic.com/7Po3dSag_xI4khGkpoWK1HF6hhy/baike/s%3D220/sign=5f4834870d3b5bb5bad727fc06d2d523/2e2eb9389b504fc2a21e8529ebdde71190ef6d5c.jpg">
+          <div class="comment-desc">
+            <div class="comment-desc__name">新垣结衣</div>
+            <div class="comment-desc__cont">あなたのことが好きです</div>
+            <div class="comment-desc__time">1月8日 13:37</div>
+          </div>
+          <div class="comment-operator">
+            
+          </div>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'comment'
+  name: 'comment',
+  data() {
+    return {
+      textarea:null
+    }
+  }
 }
 
 </script>
 <style scoped lang='less'>
+@import '../../styles/common.less';
   .comment{
-    margin-top: 20px;
+    margin-top: 50px;
+    padding-bottom: 50px;
+    h1{
+      display: inline;
+      font-size: @fs-xxl;
+      font-weight: @fw-l;
+    }
+    span{
+      margin-left: 10px;
+      color: @sub-text-color;
+      font-size: @fs-s;
+    }
+    .comment-edit{
+      margin-top: 20px;
+      width: 74%;
+      .comment-submit{
+        float: right;
+        margin-top: 15px;
+      }
+    }
+    .comment-show{
+      margin-top: 50px;
+      h2{
+        display: inline;
+        font-size: @fs;
+        font-weight: @fw-l;
+      }
+      span{
+        font-size: @fs-s;
+      }
+      .comment-list{
+        margin-top: 20px;
+        width: 74%;
+        .comment-item{
+          padding-top: 12px;
+          margin-bottom: 25px;
+          border-top: 1px solid #ededed;
+          .avatar{
+            width: 38px;
+            height: 38px;
+            border-radius: 50%;
+            float: left;
+            object-fit: cover;
+          }
+          .comment-desc{
+            margin-left: 48px;
+            font-size: @fs-s;
+            .comment-desc__name, .comment-desc__time{
+              color: @sub-text-color;
+            }
+            .comment-desc__cont, .comment-desc__time{
+              margin-top: 10px;
+            }
+
+          }
+        }
+      }
+    }
   }
 </style>
