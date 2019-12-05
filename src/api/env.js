bf37a8dcaa1193bf57acaba3a4eb92425049ad99
allenxz <13226271061@163.com>
2019-12-05 17:23:51 +0800

    Introducing environment variables

---
+++ src/api/env.js
@@ -0,0 +1,12 @@
+/*
+ * @Description: 服务器地址配置
+ * @Author: Allen Tan
+ * @Date: 2019-12-05 09:30:08
+ * @LastEditors: Allen Tan
+ * @LastEditTime: 2019-12-05 09:39:57
+ */
+
+export const server_url_prod_python = 'https://music.niracler.com:8002';
+export const server_url_prod_java = 'https://music.niracler.com:8001';
+export const server_url_dev_python = 'https://music-01.niracler.com:8002';
+export const server_url_dev_java = 'https://music-01.niracler.com:8001';
