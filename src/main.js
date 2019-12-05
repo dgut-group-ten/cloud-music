2f37d479679a5cd0815414518c649b4fcf023396
allenxz <13226271061@163.com>
2019-12-05 17:23:25 +0800

    Unity introduces the initial style in App.vue

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import ElementUI from 'element-ui';
-import './theme/index.css';
 
 Vue.use(ElementUI);
 Vue.config.productionTip = false;
