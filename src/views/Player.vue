f2241ff8dbdc92884faa74174242a926c4ecc9f3
allenxz <13226271061@163.com>
2019-12-05 17:27:41 +0800

    Roughly complete the player page

--- src/views/Player.vue
+++ src/views/Player.vue
@@ -1,10 +1,7 @@
 <template>
   <div v-if="songInfo" class="player">
     <!-- 播放标签，隐藏 -->
-    <audio id="audio" controls="controls" autoplay="autoplay" style="display:none;">
-      <source :src="songInfo.file" type="audio/mpeg" />
-      您的浏览器不支持 audio 标签。
-    </audio>
+    <audio id="audio" controls="controls" autoplay="autoplay" style="display:none;" :src="songInfo.file"></audio>
     <!-- 主体部分 -->
     <div class="player-main">
       <!-- 返回键 -->
@@ -12,9 +9,48 @@
       <!-- 播放控制面板 -->
       <div class="player-panel">
         <img class="song-avatar" :src="songInfo.cimg" alt="">
-        <div class="song-name">{{songInfo.name}}</div>
-        <div class="song-author">{{songInfo.authors[0].name}}</div>
-        <el-button class="btn" @click="contorl">{{textMapping(isPlaying)}}</el-button>
+        <div class="song-info">
+          <div class="song-name">{{songInfo.name}}</div>
+          <div class="song-author">{{songInfo.authors[0].name}}</div>
+        </div>
+        <!-- 播放进度条 -->
+        <div class="song-progress">
+          <span>{{timeFormat(currentTime)}}</span>
+          <el-progress :percentage="percentage" color="#909399" :show-text="false" :stroke-width="1"></el-progress>
+          <span>{{timeFormat(duration)}}</span>
+        </div>
+        <!-- 按钮 -->
+        <div class="song-control">
+          <a class="song-control_item" @click="contorlPlay" href="javascript:;" title="暂停">
+            <i class="icon-pause"></i>
+          </a>
+          <a class="song-control_item" href="javascript:;" title="下一首">
+            <i class="icon-next"></i>
+          </a>
+          <a class="song-control_item" @click="contorlVolume" href="javascript:;" title="关闭声音">
+            <i class="icon-volume"></i>
+          </a>
+          <div class="volume">
+            <el-slider 
+              v-model="volume" 
+              :show-tooltip="false" 
+              :min="0" 
+              :max="1" 
+              :step="0.001"
+              :disabled="!canAdjustVol"></el-slider>
+          </div>
+          <div class="right-part">
+            <a class="song-control_item" href="javascript:;" title="喜欢">
+              <i class="icon-like"></i>
+            </a>
+            <a class="song-control_item" href="javascript:;" title="删除">
+              <i class="icon-delete"></i>
+            </a>
+            <a class="song-control_item" href="javascript:;" title="更多">
+              <i class="icon-more"></i>
+            </a>
+          </div>
+        </div>
       </div>
     </div>
     <div class="player-bg_mask"></div>
@@ -26,12 +62,18 @@
 
 <script>
 import {getSingleSong} from '@/api/playlist.js';
+import { watch } from 'fs';
 export default {
   name: 'Player',
   data() {
     return {
       songInfo:null,
-      isPlaying: true,
+      currentTime:null,
+      duration: null,
+      percentage:0,
+      volume:0.3,
+      volumeCopy:0.3,
+      canAdjustVol:true
     }
   },
   created() {
@@ -39,27 +81,75 @@ export default {
     getSingleSong(sid).then((res)=>{
       this.songInfo = res;
       console.log(res)
+      // 异步执行，确保获得歌曲信息后再去获取dom结点
+      const that = this;
+      setTimeout(()=>{
+        let audio = document.querySelector('#audio');
+        // 必须要在oncanplay中调用才能获得duration的值
+        audio.oncanplay = function () {
+          that.duration = audio.duration;
+        }
+        // 监听时间变化，提供参数给进度条使用
+        audio.addEventListener("timeupdate",function(){
+          that.currentTime = audio.currentTime;
+          that.percentage = audio.currentTime/audio.duration*100;
+        })
+        // 初始化音量
+        audio.volume = 0.3;
+      },20);
     }) 
   },
   methods:{
-    contorl(){
-      let audio = document.querySelector('#audio');
-      this.isPlaying = !this.isPlaying;
-      if (audio.paused) {
-        audio.play();
-      } else{
-        audio.pause();
-      }
-    },
-    textMapping(val){
-      if(val){
-        return '暂停';
-      } else {
-        return '播放';
-      }
-    },
+    // 回到上一个浏览页面
     back(){
       this.$router.back(-1);
+    },
+    // 控制播放
+    contorlPlay(e){
+      let audio = document.querySelector('#audio');
+      if (audio.paused) {
+        audio.play();
+        e.currentTarget.children[0].classList.replace('icon-play','icon-pause');
+        e.currentTarget.title = '暂停'; 
+      } else{
+        audio.pause();
+        e.currentTarget.children[0].classList.replace('icon-pause','icon-play');
+        e.currentTarget.title = '播放';
+      }
+    },
+    // 控制音量
+    contorlVolume(e){
+      let audio = document.querySelector('#audio');
+      if (!this.canAdjustVol) {
+        this.canAdjustVol = true;
+        audio.volume = this.volumeCopy;
+        e.currentTarget.children[0].classList.replace('icon-mute','icon-volume');
+        e.currentTarget.title = '关闭声音'; 
+      } else{
+        this.canAdjustVol = false;
+        this.volumeCopy = audio.volume;
+        audio.volume = 0;
+        e.currentTarget.children[0].classList.replace('icon-volume','icon-mute');
+        e.currentTarget.title = '打开声音';
+      }
+    },
+    // 时间格式化
+    timeFormat(seconds){
+      let m = seconds/60|0;
+      let s = (seconds - m * 60)|0;
+      if(m<10){
+        m='0'+m;
+      }
+      if(s<10){
+        s='0'+s;
+      }
+      return `${m}:${s}`;
+    }
+  },
+  watch:{
+    volume(newValue, oldValue){
+      let audio = document.querySelector("#audio");
+      audio.volume = newValue; 
     }
   }
 }
@@ -96,6 +186,61 @@ export default {
           width:242px;
           height: 242px;
           object-fit: cover;
+        }
+        .song-info{
+          padding-top: 20px;
+          margin-left: 20px;
+          .song-name{
+            font-size: @fs-l;
+          }
+          .song-author{
+            margin-left: 1px;
+            margin-top: 15px;
+            font-size: @fs-s;
+            font-weight: @fw-l;
+          }
+        }
+        .song-progress{
+          margin-top: 100px;
+          margin-left: 18px;
+          span{
+            font-size: @fs-s;
+            vertical-align: middle;
+          }
+          .el-progress{
+            display: inline-block;
+            margin-left: 8px;
+            margin-right: 8px;
+            width:280px;
+            transform:translate(0,-2px);
+          }
+        }
+        .song-control{
+          margin-top: 25px;
+          margin-left: 18px;
+          .song-control_item i{
+            margin-right: 18px;
+            font-size: @fs-xxl;
+            color:@player-contorl-color;
+          }
+          & > a:nth-child(1) i{
+            margin-right: 12px;
+          }
+          .volume{
+            display: inline-block;
+            transform: translate(0, -5px);
+            height: 23px;
+            width: 80px;
+          }
+          .right-part{
+            float: right;
+            .song-control_item i{
+              margin-right: 15px;
+            }
+            & > a:nth-child(1) i{
+              margin-right: 20px;
+            }
+          }
         }
       } 
     }
