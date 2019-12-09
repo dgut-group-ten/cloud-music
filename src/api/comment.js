52935c891fd43b67623a2a178e8999a383045a86
allenxz <13226271061@163.com>
2019-12-09 17:27:43 +0800

    modify the api

--- src/api/comment.js
+++ src/api/comment.js
@@ -3,7 +3,7 @@
  * @Author: Allen Tan
  * @Date: 2019-12-09 09:42:24
  * @LastEditors: Allen Tan
- * @LastEditTime: 2019-12-09 10:30:10
+ * @LastEditTime: 2019-12-09 16:51:15
  */
 
 import axios from '@/utils/axios.js';
@@ -13,12 +13,12 @@ const baseURL = getServerURL('java');
 
 /**
  * @description: 获取评论列表
- * @param {rid：资源的id,歌单或歌曲,page:第几页} 
+ * @param {rid：资源的id,歌单或歌曲,page:第几页,ps:每页几条信息} 
  * @return: 指定资源的所有评论信息
  */
-export const getComments = function(rid,p) {
+export const getComments = function(rid,page,ps) {
   return axios({
-    url: `${baseURL}/comment?rid=${rid}&type=1&p=${p}&ps=5`,
+    url: `${baseURL}/comment?rid=${rid}&type=1&p=${page}&ps=${ps}`,
     method: 'get'
   })
 }
