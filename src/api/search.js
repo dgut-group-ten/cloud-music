94b0d1f769fcffcab94b0aed4a6150269e2d7385
allenxz <13226271061@163.com>
2019-12-12 21:29:53 +0800

    add new api

--- src/api/search.js
+++ src/api/search.js
@@ -3,7 +3,7 @@
  * @Author: Allen Tan
  * @Date: 2019-12-12 15:39:21
  * @LastEditors: Allen Tan
- * @LastEditTime: 2019-12-12 15:51:27
+ * @LastEditTime: 2019-12-12 20:14:30
  */
 
 import axios from '@/utils/axios.js';
@@ -15,11 +15,23 @@ const pythonEndURL = getServerURL('python');
 /**
  * @description: 根据关键字搜索单曲
  * @param {search:关键字} 
- * @return: 
+ * @return: 和关键字有关的单曲列表
  */
 export const searchSongs = function(search){
   return axios({
-    url: `${pythonEndURL}/song/?search=${search}`,
+    url: `${pythonEndURL}/song/?search=${search}&ps=300`,
+    method: 'get'
+  })
+}
+
+/**
+ * @description: 根据关键字搜索歌单
+ * @param {search:关键字} 
+ * @return: 和关键字有关的歌单列表
+ */
+export const searchPlaylists = function(search){
+  return axios({
+    url: `${pythonEndURL}/playlist/?search=${search}&ps=300`,
     method: 'get'
   })
 }
