e8d6857ddc0e83eca291d76ed1ce9ef927a57f76
allenxz <13226271061@163.com>
2019-12-17 21:42:58 +0800

    fix bug

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -20,7 +20,10 @@ service.interceptors.request.use(
     ];
 
     // 根据条件加入token-安全携带
-    let userInfo = JSON.parse(window.localStorage.getItem('state')).userInfo;
+    let userInfo
+    if(JSON.parse(window.localStorage.getItem('state'))){
+      userInfo = JSON.parse(window.localStorage.getItem('state')).userInfo;
+    }
     // 如果登录后，让每个请求携带token
     if (userInfo) { 
       config.headers['Token'] = userInfo.Token;
