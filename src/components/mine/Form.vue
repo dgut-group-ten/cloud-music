bfdd9e7a99718e0b1f475e6ff11daf94ae2b55de
allenxz <13226271061@163.com>
2019-12-26 21:33:35 +0800

    design the Modify module

--- src/components/mine/Form.vue
+++ src/components/mine/Form.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="wrapper">
     <el-form :model="formData" :rules="rules" ref="ruleForm" label-width="100px">
-      <el-form-item :label="labelText[type][0]" prop="oldThing">
-        <el-input v-model="formData.oldThing"></el-input>
-        <el-button class="checkCode" v-if="type === 'mail'" @click.prevent="checkCode">获取验证码</el-button>
+      <el-form-item :label="labelText[type][0]" prop="firstItem">
+        <el-input v-model="formData.firstItem"></el-input>
+        <el-button class="checkCode" v-if="type === 'mail'" @click.prevent="checkCode" :disabled="disabled">{{tip}}</el-button>
       </el-form-item>
-      <el-form-item :label="labelText[type][1]" prop="newThing">
-        <el-input v-model="formData.newThing"></el-input>
+      <el-form-item :label="labelText[type][1]" prop="secondItem">
+        <el-input v-model="formData.secondItem"></el-input>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
@@ -17,37 +17,46 @@
 </template>
 
 <script>
-import {modifyPassword} from '@/api/user.js';
+import {modifyPassword, modifyMail, checkCode} from '@/api/user.js';
 export default {
   name: 'Form',
   data() {
     return {
       formData:{
-        oldThing:'',
-        newThing: ''
+        firstItem:'',
+        secondItem: ''
       },
       rules:{
-        oldThing: [{ required: true, message: '此项不允许为空', trigger: 'blur' },],
-        newThing: [{ required: true, message: '此项不允许为空', trigger: 'blur' },],
+        firstItem: [{ required: true, message: '此项不允许为空', trigger: 'blur' },],
+        secondItem: [{ required: true, message: '此项不允许为空', trigger: 'blur' },],
       },
       labelText:{
         password:['旧密码','新密码'],
         mail:['新邮箱','验证码'],
-      }
+      },
+      send: 0,
+      tip: '获取验证码',
+      disabled: false
     }
   },
   props:['type'],
   methods: {
+    // 提交表单
     submitForm(formName) {
       this.$refs[formName].validate((valid) => {
         if (valid) {
           let fn = this.getApi();
-          fn(this.formData.oldThing,this.formData.newThing)
+          fn(this.formData.firstItem,this.formData.secondItem)
           .then(res=>{
-             this.$message(res.message);
+             this.$message({
+              message: res.message,
+              type: 'success'
+            });
+            this.resetForm('ruleForm');
           })
           .catch(err=>{
-            console.log(err)
+            let message = err.response.data.msg || err.message;
+            this.$message.error(message);
           })
         } else {
           this.$message.error('请先完善必填项!!');
@@ -55,15 +64,47 @@ export default {
         }
       });
     },
+    // 重置表单
     resetForm(formName) {
       this.$refs[formName].resetFields();
     },
+    // 根据type判断所需的api
     getApi(){
       switch(this.type){
         case 'password':
           return modifyPassword;
+        case 'mail':
+          return modifyMail;
       }
-    }
+    },
+    // 获取验证码
+    checkCode() {
+      this.send = 1;
+      this.disabled = true;
+      let num = 60;
+      let timer = setInterval(() => {
+        num--;
+        this.tip = num+'s';
+        if (num === 0) {
+          clearInterval(timer);
+          this.tip = '获取验证码';
+          this.send = 0;
+          this.disabled = false;
+        }
+      }, 1000);
+
+      checkCode(this.formData.firstItem)
+      .then(res => {
+        this.$message({
+          message: res.message,
+          type: 'success'
+        })
+      })
+      .catch(err => {
+        let message = err.response.data.msg || err.message;
+        this.$message.error(message);
+      });
+    },
   }
 }
 
---
+++ src/components/user/Reset.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="wrapper">
+    <div class="container">
+      <header>{{text}}</header>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Reset',
+  data(){
+    return {
+      text:'输入帐号'
+    }
+  }
+}
+
+</script>
+<style scoped lang='less'>
+@import '../../styles/common.less';
+  .wrapper{
+    width: 100%;
+    min-height: 650px;
+    background: #f4f4f4;
+    .container{
+      position: absolute;
+      top:50%;
+      left:50%;
+      transform: translate(-50%,-50%);
+      width:80%;
+      background:#fff;
+      header{
+        font-size: @fs-xxxl;
+        font-weight: @fw-l;
+        text-align: center;
+        margin: 30px 0;
+        border-bottom: 1px solid #fdfdfd;
+      }
+    }
+  }
+</style>
