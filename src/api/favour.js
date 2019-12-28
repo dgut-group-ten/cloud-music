23bc71cd45f2a77b997df0e0a30826e840b7c2aa
allenxz <13226271061@163.com>
2019-12-28 22:38:55 +0800

    add new api

---
+++ src/api/favour.js
@@ -0,0 +1,27 @@
+/*
+ * @Description: 收藏模块接口
+ * @Author: Allen Tan
+ * @Date: 2019-12-28 17:03:45
+ * @LastEditors  : Allen Tan
+ * @LastEditTime : 2019-12-28 17:10:56
+ */
+
+import axios from '@/utils/axios.js';
+import {getServerURL} from '@/api/env.js';
+
+const baseURL = getServerURL('python');
+
+/**
+ * @description: 收藏指定歌单
+ * @param {playlist:歌单ID} 
+ * @return: 
+ */
+export const collectPlaylist = function(playlist){
+  return axios({
+    url: `${baseURL}/playlistFav`,
+    method: 'post',
+    data:{
+      playlist
+    }
+  })
+}
